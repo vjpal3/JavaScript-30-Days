432aee303e8e540a3a90451c66e01d83abc6b7ca
vjpal3 <vrishalipal@yahoo.com>
2019-12-29 17:17:46 -0500

    Get video/pictures from webcam

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -8,13 +8,40 @@ function getVideo() {
     navigator.mediaDevices.getUserMedia({ video: true, audio: false })
         .then(localMediaStream => {
             console.log(localMediaStream);
-            video.src = window.URL.createObjectURL(localMediaStream);
+            video.srcObject = localMediaStream;
             video.play();
         })
         .catch(err => {
             console.log('Oh No!!', err);
-            
         })
 }
 
+function paintToCanvas() {
+    const width =  video.videoWidth;
+    const height =  video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+    }, 16)    
+
+}
+
+function takePhoto() {
+   // played the sound 
+    snap.currentTime = 0;
+    snap.play();
+
+    //take the data out of the canvas
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'mypic');
+    link.innerHTML = `<img src="${data}" alt="My Picture" />`;
+    strip.insertBefore(link, strip.firstChild);
+}
+
 getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
